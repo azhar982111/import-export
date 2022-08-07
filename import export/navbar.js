36041a695f923f495be1dd926ca7c4628b3e44e1
azhar982111 <100709941+azhar982111@users.noreply.github.com>
2022-08-08 00:42:47 +0530

    import export

---
+++ import export/navbar.js
@@ -0,0 +1,12 @@
+function Navbar(){
+return `<div><a href="http://127.0.0.1:5502/index.html">Home</a></div>
+<div id="right">
+    <div><a href="http://127.0.0.1:5502/recepie.html">Receipie of the day</a></div>
+    <div><a href="http://127.0.0.1:5502/signup.html">Sign up</a></div>
+    <div><a href="http://127.0.0.1:5502/login.html">Login</a></div>
+</div>`
+
+
+}
+
+export default Navbar;
